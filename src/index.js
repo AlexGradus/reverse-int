e8407558235e3ddb2a022875ec81e1f949574c23
Alex Sakalou <countr@mail.ru>
2021-08-12 02:25:46 +0300

    Feat

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
+    let line=n;
+    line=line.toString();
+    line=line.split('');
+    line=line.reverse();
+    line=line.join('').replace(/-/g,'');
+    line=Number(line);
+    return line;
   
 }
